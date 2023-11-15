1be3f03aa90068949e8224936e4f232adff4159e
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2023-11-15 11:45:20 -0300

    elemento header concluído

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import logo from './imgs/logo.svg';
 import MenuHeader from './components/header/header';
 import './styles/style.css';
 
 function App() {
   return (
     <div className="App">
-    <header className='header'>
-      <img src={logo} alt="" id='logo-header'/>
-        <p>osdsdmsa</p>
-        <MenuHeader/>
-    </header>
-      
+      <header className='header'>
+        <MenuHeader nome='Jésica' />
+      </header>
     </div>
   );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,21 @@
-import logo from './imgs/logo.svg';
+import React from 'react';
+import logo from '../../imgs/logo.svg';
 
-export default function MenuHeader() {
+export default function MenuHeader(props: any) {
     return (
-        <nav className='nav-menu-header'>
-            <ul>
-                <li>Home</li>
-                <li>Account</li>
-                <li>Dashboard</li>
-            </ul>
-        </nav>
+        <div className='sub-container-header'>
+            <div id='logo-header'>
+                <img src={logo} alt=""/>
+            </div>
+            <nav className='nav-menu-header'>
+                <ul>
+                    <li>Home</li>
+                    <li>Account</li>
+                    <li>Dashboard</li>
+                </ul>
+            </nav>
+            <p className='saudacoes'>Olá! {props.nome}</p>
+        </div>
+
     )
 }
