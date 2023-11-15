eacc7ed508906645bd09bfcecaa88272fa2fcd2e
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2023-11-15 10:50:25 -0300

    criando estrutura do header, juntamente com a esttilização

---
+++ src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import logo from './imgs/logo.svg';
+
+export default function MenuHeader() {
+    return (
+        <nav className='nav-menu-header'>
+            <ul>
+                <li>Home</li>
+                <li>Account</li>
+                <li>Dashboard</li>
+            </ul>
+        </nav>
+    )
+}
