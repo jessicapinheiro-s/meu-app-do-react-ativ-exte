d7c594107751803273e18b0fa8089e33ab82b7e9
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2023-11-26 18:09:29 -0300

    configurandobotão de selecionar respostas

--- src/Pages/dashboard.tsx
+++ src/Pages/dashboard.tsx
@@ -1,36 +1,44 @@
 import { Link } from 'react-router-dom';
 import MenuHeader from '../components/header/header';
-import React, {useState} from "react";
-import {Card} from '../components/dash/card';
+import React, { useState } from "react";
+import { Card } from '../components/dash/card';
 import { FcSearch } from "react-icons/fc";
 import { FcShop } from "react-icons/fc";
 import { FcInTransit } from "react-icons/fc";
 import { FcCompactCamera } from "react-icons/fc";
-
+import Button from '../components/buttons/button';
 
 
 
 export default function Dashboard() {
 
     const [iconItem, setIconItem] = useState('');
+
+    function answer() {
+        console.log('respondido')
+    }
     return (
         <div>
             <MenuHeader nome='Jéssica' />
             <div className='container-dash'>
-                <h1 className='title'>Nome item</h1>
+                <div className='title'>
+                    <h1>Nome item</h1>
+                </div>
                 <Card>
-                    <FcSearch className='icon'/>
+                    <FcSearch className='icon' />
                 </Card>
                 <Card>
-                    <FcShop className='icon'/>
+                    <FcShop className='icon' />
                 </Card>
                 <Card>
-                    <FcInTransit  className='icon'/>
+                    <FcInTransit className='icon' />
                 </Card>
                 <Card>
-                    <FcCompactCamera  className='icon'/>
+                    <FcCompactCamera className='icon' />
                 </Card>
-                
+                <div className='container-button'>
+                <Button className='buttonChoiceReponse' onClick={answer}>Continuar</Button>
+                </div>
             </div>
         </div>
     )
